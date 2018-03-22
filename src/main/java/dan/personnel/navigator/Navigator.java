035dc826a416ea7868378c5effe3aaeea27a2552
dan.wink <dan.wink@specsavers.com>
2018-03-22 10:50:36 +0000

    Fixed Navigator interface

--- src/main/java/dan/personnel/navigator/Navigator.java
+++ src/main/java/dan/personnel/navigator/Navigator.java
@@ -5,7 +5,7 @@ import dan.personnel.TankCrew;
 /**
  * Created by dan.wink on 01 March, 2018
  */
-public interface Navigator extends TankCrew<NavigationInstruction> {
+public interface Navigator extends TankCrew<NavigationInstruction, NavigatorUpdate> {
 
     void plotRoute(NavigationRequest... routePoints);
     void currentNavigationStatus(NavigatorUpdate navStatus);
